cf20d72c9ff31d19ce467f1057d0fcd9571c7529
timrusin <timrusin@gmail.com>
2023-01-09 15:21:29 -0600

    created news data file and newsCard

---
+++ src/data/News.js
@@ -0,0 +1,32 @@
+export const News = [
+    {
+        id: 4,
+        title: '',
+        date: '',
+        body: '' ,
+        img: '' ,
+    },
+    {
+        id: 3,
+        title: '',
+        date: '',
+        body: '' ,
+        img: '' ,
+    },
+    {
+        id: 2,
+        title: '',
+        date: '',
+        body: '' ,
+        img: '' ,
+    },
+    {
+        id: 1,
+        title: '',
+        date: '',
+        body: '' ,
+        img: '' ,
+    }
+]
+
+export default News
